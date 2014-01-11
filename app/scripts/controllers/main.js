de60fc7a12eefb14f628880125377fb0b126e37f
Giacomo Tagliabue <giacomo.tag@gmail.com>
2014-01-10 22:16:59 -0600

    added section 'learn more' to main.html

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,13 @@
 'use strict';
 
 angular.module('pamexAngularApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function($scope) {
+    $scope.learnMore = false;
+
+    $scope.toggleLearnMore = function() {
+      $scope.learnMore = !$scope.learnMore;
+    };
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('Controller: MainCtrl', function () {
+describe('Controller: MainCtrl', function() {
 
   // load the controller's module
   beforeEach(module('pamexAngularApp'));
@@ -9,14 +9,22 @@ describe('Controller: MainCtrl', function () {
     scope;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function($controller, $rootScope) {
     scope = $rootScope.$new();
     MainCtrl = $controller('MainCtrl', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
+  it('should toggle learnMore', function() {
+    expect(scope.learnMore).toBe(false);
+    scope.toggleLearnMore();
+    expect(scope.learnMore).toBe(true);
+    scope.toggleLearnMore();
+    expect(scope.learnMore).toBe(false);
+  });
+
+  it('should attach a list of awesomeThings to the scope', function() {
     expect(scope.awesomeThings.length).toBe(3);
   });
 });
